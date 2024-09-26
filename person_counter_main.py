cd22ed82808debee95ee75973b5971edf782da02
Umar91Farooq <87163599+Umar91Farooq@users.noreply.github.com>
2024-09-26 13:53:00 +0530

    Add files via upload

---
+++ person_counter_main.py
@@ -0,0 +1,54 @@
+import cv2
+import torch
+from person_counter_tracker import *
+import numpy as np
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+cap=cv2.VideoCapture('cctv.mp4')
+
+
+def POINTS(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('FRAME')
+cv2.setMouseCallback('FRAME', POINTS)
+
+tracker = Tracker()
+area_1=[(375,319),(416,381),(730,340),(576,250)]
+area1=set()
+
+while True:
+    ret,frame=cap.read()
+    frame=cv2.resize(frame,(1020,500))
+    cv2.polylines(frame,[np.array(area_1,np.int32)],True,(0,255,0),0)
+    results=model(frame)
+    list=[]
+   
+    for index,row in results.pandas().xyxy[0].iterrows():
+       x1=int(row['xmin'])
+       y1=int(row['ymin'])
+       x2=int(row['xmax'])
+       y2=int(row['ymax'])
+       b=str(row['name'])
+       if 'person' in b :
+          list.append([x1,y1,x2,y2])
+    boxes_ids=tracker.update(list)
+    for box_id in boxes_ids:
+        x,y,w,h,id=box_id
+        cv2.rectangle(frame,(x,y),(w,h),(255,0,255),2)
+        cv2.putText(frame,str(id),(x,y),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2)
+        result=cv2.pointPolygonTest(np.array(area_1,np.int32),(int(w),int(h)),False)
+        if result > 0:
+            area1.add(id)
+    p=len(area1)
+    cv2.putText(frame,"Person Counter :"+str(p),(76,64),cv2.FONT_HERSHEY_PLAIN,5,(0,255,0),3)
+    cv2.imshow('FRAME',frame)
+    if cv2.waitKey(1)&0xFF==27:
+        break 
+cap.release()
+cv2.destroyAllWindows()
+    
+    
---
+++ yolo v5 Parking_Spaces_Counter/yolov5parkingcount-main/main.py
@@ -0,0 +1,70 @@
+import cv2
+import torch
+import numpy as np
+import time
+
+points=[]
+def POINTS(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('FRAME')
+cv2.setMouseCallback('FRAME', POINTS)            
+    
+           
+
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+cap=cv2.VideoCapture('parking.mp4')
+area_1=[(24,433),(9,516),(409,490),(786,419),(720,368)]
+#area_2=[(806,354),(857,393),(998,351),(960,306),(791,332)]
+count=0
+
+while True:
+    ret,frame=cap.read()
+    if not ret:
+        break
+    frame=cv2.resize(frame,(1020,600))
+
+    results=model(frame)
+    list=[]
+    for index, row in results.pandas().xyxy[0].iterrows():
+        x1 = int(row['xmin'])
+        y1 = int(row['ymin'])
+        x2 = int(row['xmax'])
+        y2 = int(row['ymax'])
+        d=(row['name'])
+        cx=int(x1+x2)//2
+        cy=int(y1+y2)//2
+        if 'car' in d:
+            results_1=cv2.pointPolygonTest(np.array(area_1,np.int32),((cx,cy)),False)
+            if results_1>=0:
+              cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),3)
+              cv2.putText(frame,str(d),(x1,y1),cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),2)
+              list.append([cx])
+              
+            """results_2=cv2.pointPolygonTest(np.array(area_2,np.int32),((cx,cy)),False)
+            if results_2>=0:
+              cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),3)
+              cv2.putText(frame,str(d),(x1,y1),cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),2)
+              list.append([cx])"""
+            
+    cv2.polylines(frame,[np.array(area_1,np.int32)],True,(0,255,0),2)
+    #cv2.polylines(frame,[np.array(area_2,np.int32)],True,(0,255,0),2)
+    a=(12-len(list))
+    cv2.putText(frame,"Parking Slots Available: "+str(a),(50,124),cv2.FONT_HERSHEY_PLAIN,4,(0,0,255),3)
+    
+    
+    
+    cv2.imshow("FRAME",frame)
+    cv2.setMouseCallback("FRAME",POINTS)
+   
+    time.sleep(0.1)
+    if cv2.waitKey(1)&0xFF==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
